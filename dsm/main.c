54e5eda277e29662c8fcc1202c0c03ec0ee14fda
Diego Perez <diepe2892@gmail.com>
2016-05-17 22:12:59 -0600

    An example of mprotect...
    Here we create a buffer of pageSize * numPages, where pageSize = 4096 and numPages = 4, i.e, 4 pages of 4096 bytes.
    
     We protect the first page and the third, with access PROT_NONE, meaning it cannot be accessed at all.
    
     We then read the first page, which causes a segmentation fault, which is handled by our handler... the handler calculates the pageNumber that has the fault address and sets permission to that page to PROT_READ, the handler then returns to the instruction that failed and resumes it execution...
    
     We read the second page, because it was not protected nothing happens
    
     We read the third page, the same thing as with the first one happens.
    
     Then we try to write the pages, it raises a segfault but the handler does not change the access to that page to PROT_WRITE and it will loop forever between the handler and the faulting instruction... just for now, of course

--- dsm/main.c
+++ dsm/main.c
@@ -1,3 +1,4 @@
+/*
 
 #include <stdio.h>
 
@@ -5,4 +6,88 @@ int main (int argc, char *argv[])
 {
     puts("Hello from dsm central manager");
     return 0;
+}
+*/
+
+#include <signal.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/mman.h>
+
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+char *buffer;
+long firstAddress;
+int pagesize =4096;;
+int numPages = 4;
+
+static void handler(int sig, siginfo_t *si, void *unused)
+{
+    long faultAddress = (long) si->si_addr;
+    long faultingPage = (faultAddress - firstAddress) / pagesize ; //0-based
+    if (mprotect(firstAddress + pagesize * faultingPage, pagesize, PROT_READ ) == -1)
+        handle_error("mprotect");
+}
+
+int main(int argc, char *argv[])
+{
+    char *p; char a;
+
+    struct sigaction sa;
+    int i = 0;
+
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = handler;
+    if (sigaction(SIGSEGV, &sa, NULL) == -1)
+        handle_error("sigaction");
+
+
+
+    /* Allocate a buffer aligned on a page boundary;
+       initial protection is PROT_READ | PROT_WRITE */
+
+    buffer = memalign(pagesize, numPages * pagesize);
+    if (buffer == NULL)
+        handle_error("memalign");
+
+    /*printf("Start of 1st page:        0x%lx\n", (long) buffer);
+    printf("Start of 2nd page:        0x%lx\n", (long) buffer+pagesize);
+    printf("Start of 3rd page:        0x%lx\n", (long) buffer+2*pagesize);
+    printf("Start of 4th page:        0x%lx\n", (long) buffer+3*pagesize);*/
+
+    printf("Start of 1st page:        %lu\n", (long) buffer);
+    printf("Start of 2nd page:        %lu\n", (long) buffer+pagesize);
+    printf("Start of 3rd page:        %lu\n", (long) buffer+2*pagesize);
+    printf("Start of 4th page:        %lu\n", (long) buffer+3*pagesize);
+
+    //protecting the memory from [buffer, buffer + pagesize], i.e, the first page
+    firstAddress = buffer;
+    if (mprotect(buffer + pagesize * 0, pagesize, PROT_NONE ) == -1)
+        handle_error("mprotect");
+
+    //protects the third
+    if (mprotect(buffer + pagesize * 2, pagesize, PROT_NONE ) == -1)
+        handle_error("mprotect");
+
+    // this will cause the first read to be catched by the handler, the second one does nothing because the memory is not protected
+    // then the third causes a fault
+    while(i < numPages){
+        p = buffer+(pagesize*i++); // moving from page to page
+        a = *p; //trying to read the memory
+    }
+    i = 0;
+
+    // will fail for now...
+    while(i < numPages){
+        p = buffer+(pagesize*i++);
+        *(p) = 'a'; //trying to write the memory
+    }
+
+
+    printf("Loop completed\n");
+    exit(EXIT_SUCCESS);
 }
