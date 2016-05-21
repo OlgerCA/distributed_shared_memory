9da34c610152b5f55520bdcc5912fcdec821fe4d
Olger Calderón <olcala28@yahoo.es>
2016-05-20 18:32:53 -0600

    Added memory mapping logic and page fault handler in client node_init.

--- dsm_library/ClientPageEntry.h
+++ dsm_library/ClientPageEntry.h
@@ -2,10 +2,12 @@
 #ifndef DISTRIBUTED_SHARED_MEMORY_CLIENTPAGEENTRY_H
 #define DISTRIBUTED_SHARED_MEMORY_CLIENTPAGEENTRY_H
 
+#include <stdbool.h>
+
 typedef struct {
-    int present;
-    int ownership;
-    int isReadOnly;
+    bool present;
+    bool ownership;
+    bool isReadOnly;
 } ClientPageEntry;
 
 #endif
--- dsm_library/DSMClient.c
+++ dsm_library/DSMClient.c
@@ -1,14 +1,38 @@
+#include <sys/mman.h>
 #include <Requests.h>
 #include <stdlib.h>
 #include <Responses.h>
 #include <NetworkInfo.h>
+#include <unistd.h>
+#include <signal.h>
 #include "DSMClient.h"
 #include "ClientRequest.h"
+#include "ClientPageEntry.h"
 
 // This file should contain all memory protection and mapping logic.
 // This is the only API used by client applications.
 
 static int nodeId;
+
+static ClientPageEntry* pages;
+static long totalNumberOfPages;
+
+static void* addressSpace;
+static size_t addressSpaceLength;
+
+static void handle_page_fault(int sig, siginfo_t *si, void *unused)
+{
+    long faultAddress = (long) si->si_addr;
+    long faultingPage = (faultAddress - (long) addressSpace) / getpagesize() ; //0-based
+
+    if (faultingPage > -1 && faultingPage < totalNumberOfPages) {
+        ClientPageEntry* page = &(pages[faultingPage]);
+        // TODO How dow we know if it was a read or a write? handle every possible case
+
+    } else {
+        // TODO how we should pass this fault to the OS?
+    }
+}
 
 // From arguments we should receive server ip address and port, and also the client port to use.
 int DSM_node_init(int *argc, char ***argv, int *nodes, int *nid) {
@@ -38,11 +62,26 @@ int DSM_node_init(int *argc, char ***argv, int *nodes, int *nid) {
     *nid = response->assignedNodeId;
     nodeId = response->assignedNodeId;
 
-    // TODO Init page table and other data structures
-
+    totalNumberOfPages = response->numberOfPages;
+    pages = (ClientPageEntry*) malloc(sizeof(ClientPageEntry) * totalNumberOfPages);
     free(request);
     free(response);
-    return 0;
+
+    int i;
+    for (i = 0; i < totalNumberOfPages; i++)
+        pages[i].present = false;
+
+    addressSpaceLength = (size_t) (getpagesize() * totalNumberOfPages);
+    addressSpace = mmap(NULL, addressSpaceLength, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, -1);
+
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = handle_page_fault;
+    if (sigaction(SIGSEGV, &sa, NULL) == -1)
+        return -1;
+
+    return mprotect(addressSpace, addressSpaceLength, PROT_NONE) == -1;
 }
 
 int DSM_node_exit(void) {
@@ -56,8 +95,9 @@ int DSM_node_exit(void) {
     free(response);
     nodeId = 0;
 
-    // TODO Free Page table
+    // TODO Free Page table and everything else
 
+    free(pages);
     return errorCode != 0;
 }
 
