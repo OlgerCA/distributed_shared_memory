a7659dcbf5170e691b67df4db7368a0363e9a150
Olger Calderón <olcala28@yahoo.es>
2016-05-28 22:17:55 -0600

    Added first test application.

--- application_1/main.c
+++ application_1/main.c
@@ -1,16 +1,42 @@
 
 #include <stdio.h>
 #include <DSMClient.h>
+#define ARRAY_SIZE 4096
 
 int main (int argc, char *argv[])
 {
-    puts("Hello from application_1");
+    int nodes, nid, i;
+    if (DSM_node_init(&argc, &argv, &nodes, &nid) != 0) {
+        puts("Error when initializing the node...");
+        return -1;
+    }
 
-    int numberOfNodes = 0;
-    int nodeId = 0;
-    DSM_node_init(&argc, &argv, &numberOfNodes, &nodeId);
+    int* numbers = (int*) DSM_alloc(sizeof(int) * ARRAY_SIZE);
+    int* partialResults = (int*) DSM_alloc(sizeof(int) * nodes);
+    if (nid == 0) {
+        for (i = 0; i < ARRAY_SIZE; i++)
+            numbers[i] = i;
+    }
 
+    DSM_barrier(0);
+    int partialResult = 0;
+    int assignedElements = ARRAY_SIZE / nodes;
+    for (i = nid * assignedElements; i < (nid + 1) * assignedElements; i++)
+        partialResult += numbers[i];
+    partialResults[nid] = partialResult;
 
-    DSM_node_exit();
+    DSM_barrier(1);
+    if (nid == 0)  {
+        int total = 0;
+        for (i = 0; i < nodes; i++)
+            total += partialResults[i];
+        printf("The final result is: %d\n", total);
+    }
+
+    DSM_barrier(2);
+    if (DSM_node_exit() != 0) {
+        puts("Error when exiting from node...");
+        return -1;
+    }
     return 0;
 }
--- application_2/main.c
+++ application_2/main.c
@@ -5,5 +5,13 @@
 int main (int argc, char *argv[])
 {
     puts("Hello from application_2");
+    int numberOfNodes = 0;
+    int nodeId = 0;
+    DSM_node_init(&argc, &argv, &numberOfNodes, &nodeId);
+    int* myArray = DSM_alloc(sizeof(int) * 1024);
+    int result = myArray[1024];
+    myArray[1] = 1;
+    DSM_node_exit();
+    return 0;
     return 0;
 }
