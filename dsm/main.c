762f8efa3cd5ae9019fb8d42eb991246c7112bfa
andresme <andres.me@gmail.com>
2016-05-28 19:44:02 -0600

    Argument parsing

--- dsm/main.c
+++ dsm/main.c
@@ -15,6 +15,8 @@ int main (int argc, char *argv[])
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/mman.h>
+#include <getopt.h>
+#include <stdbool.h>
 #include "FileReader.h"
 
 
@@ -42,12 +44,67 @@ static void handler(int sig, siginfo_t *si, void *unused)
 
 int main(int argc, char *argv[])
 {
-//    ClientList* clients = readFile("/home/andres/test.txt");
+//    char ch;
+//    //default values for arguments
+//    char *hostFile = "./hosts";
+//    char *logFile = "./logFile";
+//    int processCopies = 1;
+//    char *processName;
+//    char *otherArguments;
+//
+//    //magic happens here
+//    while((ch = (char)getopt(argc, argv, "H:l:n:")) != EOF){
+//        switch(ch) {
+//            case 'H':
+//                hostFile = optarg;
+//                break;
+//            case 'l':
+//                logFile = optarg;
+//                break;
+//            case 'n':
+//                processCopies = (int) strtol(optarg, (char **)NULL, 10);
+//                break;
+//            default:
+//                fprintf(stderr,"Unknown option: '%s'\n", optarg);
+//                exit(1);
+//        }
+//    }
+//
+//    int index;
+//    int totalLength = 0;
+//
+//    //get the process name
+//    processName = malloc(strlen(argv[optind]) * sizeof(char));
+//    strcpy(processName, argv[optind]);
+//    optind++;
+//
+//    //get the size of other args to use a single char* to manage them
+//    for (index = optind; index < argc; index++){
+//        totalLength += strlen(argv[index]) + 1;
+//    }
+//
+//    //actually copy the first extra argument
+//    otherArguments = malloc(totalLength * sizeof(char));
+//    strcpy(otherArguments, argv[optind]);
+//    optind++;
+//
+//    //concat the other arguments
+//    for (index = optind; index < argc; index++){
+//        strcat(otherArguments, " ");
+//        strcat(otherArguments, argv[index]);
+//    }
+//
+//    //print the arguments :)
+//    printf("Arguments: %s, %s, %d\n", hostFile, logFile, processCopies);
+//    printf("Executable: %s\n", processName);
+//    printf("Non-option arguments: %s\n", otherArguments);
+//
+//    //read the host file
+//    ClientList* clients = readFile(hostFile);
 //    int i = 0;
 //    for(i; i < clients->size; i++){
 //        printf("%s:%s \n", clients->clients[i]->clientIP, clients->clients[i]->clientPort);
 //    }
-
 
     char *p; char a;
     int* anotherPointer;// = (int*) (malloc(sizeof(int) * 5));
