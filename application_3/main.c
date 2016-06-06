c548de68d205bb48e4e016103d60fd8ca424eb65
Diego Perez <diepe2892@gmail.com>
2016-06-05 23:46:49 -0600

    Adding application 3 counting powers of two in random array

--- application_3/main.c
+++ application_3/main.c
@@ -1,103 +1,74 @@
 
 #include <stdio.h>
 #include <DSMClient.h>
-#include <math.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+#include <math.h>
 
+#define ARRAY_SIZE 8192
 #define MAX_VALUE 10000
 
-// Merge Sort related functions
+int is2Exp(int n){
+    double square = sqrt(n);
+    int intSquare = (int) square;
 
-/* Returns true if x should be before y */
-bool asc_order_comparer(int x, int y) { return x <= y; }
-bool desc_order_comparer(int x, int y) { return x > y; }
-
-void merge(int *A, int p, int q, int r, bool (*comparison_predicate)(int,int), int sentinel)
-{
-    int i, j, k;
-    int n1 = (q - p + 1);
-    int n2 = (r - q);
-    int* L = malloc(sizeof(int) * (n1 + 1));
-    int* R = malloc(sizeof(int) * (n2 + 1));
-    for (i = 0; i < n1; i++)
-        L[i] = A[p + i];
-    for (j = 0; j < n2; j++)
-        R[j] = A[q + j + 1];
-    L[n1] = sentinel;
-    R[n2] = sentinel;
-    i = 0;
-    j = 0;
-    for (k = p; k <= r; k++) {
-        if (comparison_predicate(L[i], R[j])) {
-            A[k] = L[i];
-            i++;
-        } else {
-            A[k] = R[j];
-            j++;
-        }
-    }
-    free(L);
-    free(R);
-}
-
-void msort_internal(int *A, int p, int r, bool (*comparison_predicate)(int,int), int sentinel)
-{
-    if (p < r) {
-        int q = ((p + r) / 2);
-        msort_internal(A, p, q, comparison_predicate, sentinel);
-        msort_internal(A, q + 1, r, comparison_predicate, sentinel);
-        merge(A, p, q, r, comparison_predicate, sentinel);
-    }
-}
-
-void msort(int *A, int N, bool (*comparison_predicate)(int,int), int sentinel)
-{
-    msort_internal(A, 0, N-1, comparison_predicate, sentinel);
-}
-
-
-// Shear Sort related functions
-
-// For even rows, elements are sorted in ascending order. For odd rows, elements are sorted in descending order.
-void sort_rows_alternate_direction(int **A, int M)
-{
-    int i;
-    for (i = 0; i < M; i++) {
-        if (i % 2 == 0)
-            msort(A[i], M, asc_order_comparer, MAX_VALUE);
-        else
-            msort(A[i], M, desc_order_comparer, -1);
-    }
-}
-
-// The elements of each column are copied to a one dimensional array so they can be processed by msort(). Each thread has its own auxiliar array for doing this.
-void sort_columns(int **A, int M)
-{
-    int i, j;
-    int* columnCopy = malloc(sizeof(int) * M);
-    for (j = 0; j < M; j++) {
-        for (i = 0; i < M; i++)
-            columnCopy[i] = A[i][j];
-        msort(columnCopy, M, asc_order_comparer, MAX_VALUE);
-        for (i = 0; i < M; i++)
-            A[i][j] = columnCopy[i];
-    }
-    free(columnCopy);
-}
-
-// Function called from shear_main.cpp.
-void shear_sort(int **A, int M)
-{
-    int i;
-    for (i = 0; i < log2(M * M); i++) {
-        sort_rows_alternate_direction(A, M);
-        sort_columns(A, M);
-    }
+    return (square - intSquare == 0);
 }
 
 int main (int argc, char *argv[])
 {
-    puts("Hello from application_3");
+    int nodes, nid, i;
+    if (DSM_node_init(&argc, &argv, &nodes, &nid) != 0) {
+        puts("Error when initializing the node...");
+        return -1;
+    }
+
+    int* numbers = (int*) DSM_alloc(sizeof(int) * ARRAY_SIZE);
+    int* partialResults = (int*) DSM_alloc(sizeof(int) * nodes);
+    int secuentialTotal = 0;
+    srand48(time(NULL));
+    if (nid == 0) {
+        for (i = 0; i < ARRAY_SIZE; i++) {
+            numbers[i] = lrand48() % MAX_VALUE;;
+            secuentialTotal += is2Exp(numbers[i]);
+        }
+        printf("The number of power of tow numbers sequentially found is %d\n", secuentialTotal);
+    }
+
+
+    DSM_barrier(0);
+    int partialResult = 0;
+    int assignedElements = ARRAY_SIZE / nodes;
+    for (i = nid * assignedElements; i < (nid + 1) * assignedElements; i++) {
+        partialResult += is2Exp(numbers[i]);
+    }
+
+    printf("The number of power of tow numbers found by: %d is: %d\n", nid, partialResult);
+
+    for(i = 0; i < nodes; i++){
+        if(nid == i){
+            partialResults[nid] = partialResult;
+            printf("in node: %d partialResult[%i]: %d\n", nid, i, partialResults[nid]);
+        }
+        DSM_barrier(i+1);
+    }
+
+    DSM_barrier(nodes+2);
+    if (nid == 0)  {
+        int total = 0;
+        for (i = 0; i < nodes; i++) {
+            total += partialResults[i];
+            printf("Value reported by %d is %d\n", i, partialResults[i]);
+        }
+
+        printf("The number of power of tow numbers found distributively in the array is: %d\n", total);
+    }
+
+    DSM_barrier(nodes+3);
+    if (DSM_node_exit() != 0) {
+        puts("Error when exiting from node...");
+        return -1;
+    }
     return 0;
 }
